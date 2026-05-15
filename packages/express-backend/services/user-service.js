a62be7e0a7b1e4a8b4e6ac76036676ffc40a05aa
Brian Gomez <briangmz1568@gmail.com>
2026-05-14 23:22:06 -0700

    Finished many important basic functions of filtering users in user-service.js

--- packages/express-backend/services/user-service.js
+++ packages/express-backend/services/user-service.js
@@ -1,11 +1,58 @@
-import mongoose from "mongoose";
 import UserModel from "../models/users.js";
 
-mongoose.set("debug", true);
+function createUser(user) {
+  const userToAdd = new UserModel(user);
+  return userToAdd.save();
+}
 
-mongoose
-  .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .catch((error) => console.log(error));
+function getUsers() {
+  return UserModel.find()
+    .populate("favoriteRestaurants")
+    .populate("addedRestaurants.restaurant");
+}
+
+function findUserById(id) {
+  return UserModel.findById(id)
+    .populate("favoriteRestaurants")
+    .populate("addedRestaurants.restaurant");
+}
+
+function findUserByUsername(username) {
+  return UserModel.findOne({ username });
+}
+
+function addFavoriteRestaurant(userId, restaurantId) {
+  return UserModel.findByIdAndUpdate(
+    userId,
+    { $addToSet: { favoriteRestaurants: restaurantId } },
+    { new: true }
+  )
+    .populate("favoriteRestaurants")
+    .populate("addedRestaurants.restaurant");
+}
+
+function addRestaurantWithNotes(userId, restaurantId, notes) {
+  return UserModel.findByIdAndUpdate(
+    userId,
+    {
+      $push: {
+        addedRestaurants: {
+          restaurant: restaurantId,
+          notes
+        }
+      }
+    },
+    { new: true }
+  )
+    .populate("favoriteRestaurants")
+    .populate("addedRestaurants.restaurant");
+}
+
+export default {
+  createUser,
+  getUsers,
+  findUserById,
+  findUserByUsername,
+  addFavoriteRestaurant,
+  addRestaurantWithNotes
+};
