4591798baea41d03a33f71ed04f85e09571b89b9
Brian Gomez <briangmz1568@gmail.com>
2026-05-27 02:39:21 -0700

    Added the filters to be case-insensitive and ability to sort

--- packages/express-backend/services/restaurant-service.js
+++ packages/express-backend/services/restaurant-service.js
@@ -8,18 +8,30 @@ function getRestaurants(
   reviewStars,
   reviewCount,
   averagePriceSpent,
-  occasion
+  occasion,
+  sortBy
 ) {
-  // TODO implement number filters
   const query = {};
+  // Case-insensitive cuisine search
   if (cuisine) {
-    query.cuisine = cuisine;
+    query.cuisine = {
+      $regex: cuisine,
+      $options: "i"
+    };
   }
+  // Case-insensitive partial restaurant name search
   if (name) {
-    query.name = name;
+    query.name = {
+      $regex: name,
+      $options: "i"
+    };
   }
+  // Case-insensitive partial address search
   if (address) {
-    query.address = address;
+    query.address = {
+      $regex: address,
+      $options: "i"
+    };
   }
   if (priceRange) {
     query.priceRange = priceRange;
@@ -40,10 +52,33 @@ function getRestaurants(
       $lte: averagePriceSpent + 2
     };
   }
+  // Case-insensitive occasion search
   if (occasion) {
-    query.occasion = occasion;
+    query.occasion = {
+      $regex: occasion,
+      $options: "i"
+    };
   }
-  return restaurantModel.find(query);
+  // Sorting options for restaurant results
+  const sortQuery = {};
+
+  if (sortBy === "rating") {
+    sortQuery.reviewStars = -1;
+  }
+
+  if (sortBy === "reviews") {
+    sortQuery.reviewCount = -1;
+  }
+
+  if (sortBy === "priceLow") {
+    sortQuery.averagePriceSpent = 1;
+  }
+
+  if (sortBy === "priceHigh") {
+    sortQuery.averagePriceSpent = -1;
+  }
+
+  return restaurantModel.find(query).sort(sortQuery);
 }
 
 function addRestaurant(restaurant) {
